825310d42ac8592a4cfe0cbbfe955d1af8e985ba
GuilhQueiroz <guiguimaraes.dev@gmail.com>
2024-09-14 19:50:45 -0300

    Endpoit de clientes

--- src/main/java/com/stocker/api/domain/repository/CustomerRepository.java
+++ src/main/java/com/stocker/api/domain/repository/CustomerRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.UUID;
 
 public interface CustomerRepository extends MongoRepository<Customer, UUID> {
+    Customer findByCpf(String cpf);
 }
