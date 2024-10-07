42ffaa3a33bc0071e53a7f23c9d92896d86581b5
davimarques <davibaterabatera@hotmail.com>
2024-10-07 11:02:23 -0300

    test: Implements UserService test base

---
+++ src/test/java/com/stocker/api/service/impl/UserServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.stocker.api.service.impl;
+
+import com.stocker.api.domain.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void findByIdOrElseThrow() {
+
+    }
+}
